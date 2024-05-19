6f1b7e0327e8e76557cbcdf0ac353a29a82123c9
Miguel Dias <miguel.dias@estudante.ufjf.br>
2024-05-19 16:46:19 -0300

    Som e start button adicionados

--- src/Componentes/SingleplayerGame/SinglePlayer.jsx
+++ src/Componentes/SingleplayerGame/SinglePlayer.jsx
@@ -2,12 +2,13 @@ import React, { Component } from 'react';
 import './SinglePlayer.css';
 import Player from './Player/Player';
 import sprite from '../../Assets/sprite.png';
+import michael from '../../Sons/michael.mp3';
+import musica from '../../Sons/musica8bitSemCopyRight.mp3';
 
 function IndicadorClique(props) {
   
   return (
     <div style={{ visibility: props.visivel }} onClick={props.blink} className='IndicadorVisual'>
-      Equilibrar
     </div>
   );
 }
@@ -15,7 +16,10 @@ function IndicadorClique(props) {
 class SinglePlayerGame extends Component {
   constructor(props) {
     super(props);
-    this.state = { contadorTempo: 0, pontos: 0, pula: false, visivel: 'hidden' };
+    this.state = { 
+      start: false,
+      contadorTempo: 0, pontos: 0, pula: false, 
+      visivel: ['hidden','visible','hidden'] };
     this.blink = this.blink.bind(this);
     this.back = this.back.bind(this);
   }
@@ -34,22 +38,28 @@ class SinglePlayerGame extends Component {
     0, 0, 1, 1, 0, 0, 1, 1, 0, 0, 0,
     1, 1, 0, 0, 0, 1, 1, 0, 0, 1, 5
   ];
+  start(){
 
-  componentDidMount() {
-    console.log('mounted');
     this.interval = setInterval(() => {
       this.setState((prevState) => ({ contadorTempo: prevState.contadorTempo + 1 }), () => {
         let contador = this.state.contadorTempo;
         if (this.intervalos[contador] === 1) {
-          this.setState({ visivel: 'visible' });
-        } else if (this.intervalos[contador] === 5) {
+          this.setState({ visivel: ['visible','visible','visible'] });
+        } 
+        
+        else if (this.intervalos[contador] === 5) {
           window.alert('Fim de jogo');
           clearInterval(this.interval);
-        } else {
-          this.setState({ visivel: 'hidden' });
-        }
+        } 
+          
+        else {
+            this.setState({ visivel: ['hidden','hidden','hidden'] });
+          }
       });
     }, 500);
+  }
+  componentDidMount() {
+    console.log('mounted');
   }
 
   componentWillUnmount() {
@@ -61,7 +71,17 @@ class SinglePlayerGame extends Component {
   }
 
   blink() {
-    this.setState({ pula: true });
+    if(this.state.start === false){
+      this.start();
+      let audio = document.getElementById('audio2');
+      audio.play();
+      this.setState({ start: true });
+    }
+    this.setState({ pula: true, visivel: ['hidden','hidden','hidden'] });
+    let audio = document.getElementById('audio');
+    audio.play();
+
+
     this.setState((prevState) => ({ pontos: this.state.pontos + 1 }));
     setTimeout(() => {
       this.setState({ pula: false });
@@ -81,9 +101,20 @@ class SinglePlayerGame extends Component {
           {/* <img src={sprite} alt="" /> */}
         </header>
         <section className='GameArea'>
-          <Player pula={this.state.pula}/>
-          <IndicadorClique blink={this.blink} visivel={this.state.visivel} />
+          <section className='Game'>
+            <div className='GameBackground'></div>
+            <Player pula={this.state.pula}/>
+
+          </section>
+          <section className='GameControls'>  
+            <IndicadorClique blink={this.blink} visivel={this.state.visivel[0]} />
+            <IndicadorClique blink={this.blink} visivel={this.state.visivel[1]} />
+            <IndicadorClique blink={this.blink} visivel={this.state.visivel[0]} />
+          </section>
         </section>
+        <audio  id = 'audio' src={michael} />
+        <audio  id = 'audio2' src={musica} />
+
       </main>
     );
   }
