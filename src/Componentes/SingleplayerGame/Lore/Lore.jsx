01d51e68d1a44fa93c3aff489c2c82170501cb55
Cauã Moreno <cauam7344@gmail.com>
2024-05-23 20:32:46 -0300

    configura lore e tutorial

--- src/Componentes/SingleplayerGame/Lore/Lore.jsx
+++ src/Componentes/SingleplayerGame/Lore/Lore.jsx
@@ -6,128 +6,39 @@ import { Swiper, SwiperSlide } from 'swiper/react';
 import 'swiper/css';
 import { useCookies } from 'react-cookie';
 import { Pagination } from 'swiper/modules';
+import { LorePrimeira } from './LorePrimeira';
+import { Tutorial } from './Tutorial';
+
 export function Lore( props) {
-
+    
     const [cookies, setCookie] = useCookies(['dadosCookie']);
-    console.log(`cookiesLore`, cookies);
-
+    let dados = cookies.dadosCookie;
+    const fezTutorial = dados.fezTutorial;
+    const faseAtual = props.fase;
     
-    let handleClick = () => {
+    console.log(fezTutorial)
+    console.log(faseAtual)
 
-        if(cookies.dadosCookie.fezTutorial === false)
+    if(fezTutorial == false && faseAtual == 0)
         {
-            let newBiscuit = cookies.dadosCookie;
-            newBiscuit.fezTutorial = true;
-
-            setCookie('dadosCookie', newBiscuit);
+            console.log('Entrou!')
+            return (
+                <Tutorial></Tutorial>
+            )
         }
-
-        let x = document.getElementById('lore');
-        x.classList.add('fadeout');
-        setTimeout(() => {
-            let x = document.getElementById('lore');
-            let y = document.getElementById('back');
-            y.style.display = 'none';
-            x.style.display = 'none';
-        },501)
-        setTimeout(() => {props.click()}, 505)
         
-    }
-    
-    
-    let _display;
-    if(props.fase === 0 /*|| cookies.lorepassou == true*/){
-        _display = 'none';
-    }
+        switch (faseAtual) {
+        case 1: 
+        return (
+            <LorePrimeira></LorePrimeira>
+        )
+        case 2:
+            return (
+                <LorePrimeira></LorePrimeira>
+            )
 
-    if(cookies.dadosCookie.fezTutorial === false)
-    {
-    return (
-        <>
-         <div id='back' className='background'>
                 
-        </div>
-        <div style={{display:_display}} className = 'Lore fadein' id='lore'>
-           
-           <Swiper
-               className='swiper'
-               spaceBetween={0}
-               slidesPerView={1}
-               modules={[Pagination]}
-               pagination={true}
-               onSlideChange={() => console.log('slide change')}
-               onSwiper={(swiper) => console.log(swiper)}>
-
-
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p className='text'>Clique nos botões certos na hora certa</p>
-                      
-                       <img src={lore1}></img>
-
-                   </section>
-               </SwiperSlide>
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p  className='text'>Um dirincando e apareceram duas criaturas que a desafiaram</p>
-                       <img src={lore2}></img>
-                   </section>
-               </SwiperSlide>
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p  className='text'>Raquel, determinada e sem nada pra fazer, aceitou a hora! </p>
-                       <button onClick={handleClick}>Start</button>
-                   </section>
-               </SwiperSlide>
-           </Swiper>
-       </div>
-        </>
-        
-        
-    );
+        default:
+            break;
+        }
     }
-
-    return (
-        <>
-         <div id='back' className='background'>
-                
-        </div>
-        <div style={{display:_display}} className = 'Lore fadein' id='lore'>
-           
-           <Swiper
-               className='swiper'
-               spaceBetween={0}
-               slidesPerView={1}
-               modules={[Pagination]}
-               pagination={true}
-               onSlideChange={() => console.log('slide change')}
-               onSwiper={(swiper) => console.log(swiper)}>
-
-
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p className='text'>Era uma vez uma menina chamada raquel janaina, conhecida por sua coragem e decisões imaturas. </p>
-                      
-                       <img src={lore1}></img>
-
-                   </section>
-               </SwiperSlide>
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p  className='text'>Um dirincando e apareceram duas criaturas que a desafiaram</p>
-                       <img src={lore2}></img>
-                   </section>
-               </SwiperSlide>
-               <SwiperSlide className='slide'>
-                   <section>
-                       <p  className='text'>Raquel, determinada e sem nada pra fazer, aceitou a hora! </p>
-                       <button onClick={handleClick}>Start</button>
-                   </section>
-               </SwiperSlide>
-           </Swiper>
-       </div>
-        </>
-        
-        
-    );
-}
